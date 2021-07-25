4e8a2ab87429b36c7438d4af01040c30df7021f6
bianyunjian <626680108@qq.com>
2021-07-25 12:35:33 +0800

    update entity definition

--- subquery-starter/src/mappings/mappingHandlers.ts
+++ subquery-starter/src/mappings/mappingHandlers.ts
@@ -7,7 +7,7 @@ export async function handleBlock(block: SubstrateBlock): Promise<void> {
     //Create a new starterEntity with ID using block hash
     let record = new StarterEntity(block.block.header.hash.toString());
     //Record block number
-    record.field1 = block.block.header.number.toNumber();
+    record.block_number = block.block.header.number.toNumber();
     await record.save();
 }
 
@@ -15,18 +15,18 @@ export async function handleEvent(event: SubstrateEvent): Promise<void> {
     const {event: {data: [account, balance]}} = event;
     //Retrieve the record by its ID
     const record = await StarterEntity.get(event.extrinsic.block.block.header.hash.toString());
-    record.field2 = account.toString();
+    record.account = account.toString();
     //Big integer type Balance of a transfer event
-    record.field3 = (balance as Balance).toBigInt();
+    record.balance = (balance as Balance).toBigInt();
     await record.save();
 }
 
 export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
     const record = await StarterEntity.get(extrinsic.block.block.header.hash.toString());
     //Date type timestamp
-    record.field4 = extrinsic.block.timestamp;
+    record.timestamp = extrinsic.block.timestamp;
     //Boolean tyep
-    record.field5 = true;
+    record.flag = true;
     await record.save();
 }
 
